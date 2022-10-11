2c3264e990ccdbbe5b24ebc16129c4210916a3c4
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-10-11 11:51:35 +0600

    added the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './Components/Blog/Blog';
+import Chart from './Components/Chart/Chart';
+import Home from './Components/Home/Home';
+import Main from './Components/Main/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/home',
+          element:<Home></Home>,
+        },
+        {
+           path:'/chart',
+           element:<Chart></Chart>
+        },
+        {
+          path:'/blog',
+          element:<Blog></Blog>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className='header'>
+            <Link to='/home'>Home</Link>
+            <Link to='/chart'>Chart</Link>
+            <Link to='/blog'>Blog</Link>
+        </nav>
+    );
+};
+
+export default Header;
